cd8976cd4aa750a7e9a7535507c8304d583cc5d2
Liz Tom & alex dinnel <student@epicodus.com>
2014-02-21 14:35:39 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+var LetterValue = function(word){
+  var onePointLetter = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T', 1];
+  var twoPointLetter = ['D', 'G', 2];  
+  var threePointLetter = ['B', 'C', 'M', 'P', 3];
+  var fourPointLetter = ['F', 'H', 'V', 'W', 'Y', 4];
+  var fivePointLetter = ['K', 5];
+  var eightPointLetter = ['J', 'K', 8];
+  var tenPointLetter = ['Q', 'Z', 10];
+  var letterArray = [onePointLetter, twoPointLetter, threePointLetter, fourPointLetter, fivePointLetter, eightPointLetter, tenPointLetter];
+
+    for(var i  = 0; i < letterArray.length; i++){
+      for(var x  = 0; x < letterArray[i].length-1; x++){
+        if (word === letterArray[i][x]) {
+          return letterArray[i][letterArray[i].length-1]; 
+        }
+      };
+    };
+};
+
+var Scrabble = function(word){
+  word = word.toString().toUpperCase();
+  var userArray = word.split(""); 
+  var totalPoints = 0;
+  var points = 0;
+
+  userArray.forEach(function(letters){
+     points = LetterValue(letters);
+     totalPoints += points;
+  });
+
+  if (totalPoints % 1 != 0){
+    return "Invalid Entry";
+  } else {
+  return totalPoints;
+  }
+};
+
+$(document).ready(function() {
+  $("form#scrabble").submit(function(event) {
+    var userWord = $('input#wordInput').val();
+    var finalPoints = Scrabble(userWord);
+
+    $(".finalResult").text("Your word is worth: " + finalPoints + " points!");
+    $("#finalResult").show();
+    this.reset();
+    event.preventDefault();
+  });
+});
+
---
+++ spec/specs.js
@@ -0,0 +1,35 @@
+describe('LetterValue', function() {
+  it('Returns 1 if A, E, I, O, U, L, N, R, S, T', function() {
+    LetterValue("A").should.equal(1)
+  });
+  it('Returns 2 if D or G is entered', function(){
+    LetterValue("D").should.equal(2)
+  });
+  it('Returns 3 if B, C, M, P', function(){
+    LetterValue("B").should.equal(3)
+  });
+  it('Returns 4 if F, H, V, W, Y', function(){
+    LetterValue("F").should.equal(4)
+  });
+  it('Returns 5 if K', function(){
+    LetterValue("K").should.equal(5)
+  });
+  it('Returns 8 if J, X', function(){
+    LetterValue("J").should.equal(8)
+  });
+  it('Returns 10 if Q, Z', function(){
+    LetterValue("Q").should.equal(10)
+  });
+});
+
+describe('Scrabble', function(){
+  it('Returns the sum of points for a word', function(){
+    Scrabble("COW").should.equal(8)
+  });
+  it('Capitalizes lower case words', function(){
+    Scrabble("qkf").should.equal(19)
+  });
+  it('Returns Invalid Input if the user enters an invalid character', function(){
+    Scrabble("hello there!").should.equal('Invalid Entry')
+  });
+});
